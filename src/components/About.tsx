00ce561a841a4466b684a312554b8e0a57c6a1a3
Denisz Becskei <denisz.becskei@frontendart.com>
2025-06-13 09:30:25 +0200

    Improving Animations and Adding Extra Things

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,104 +1,99 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { motion } from "framer-motion";
 import styled from "styled-components";
 
 interface Skill {
-    id: number;
-    concept: string;
-    experience: number;
+  id: number;
+  concept: string;
+  experience: number;
 }
 
 const skills: Skill[] = [
-    { id: 1, concept: "Angular", experience: 2021 },
-    { id: 2, concept: "TypeScript", experience: 2021 },
-    { id: 3, concept: "Java", experience: 2020 },
-    { id: 4, concept: "Spring Boot", experience: 2021 },
-    { id: 5, concept: "HTML/SCSS", experience: 2020 },
-    { id: 6, concept: "C#", experience: 2021 },
-    { id: 7, concept: "Unity", experience: 2021 },
-    { id: 8, concept: "Python", experience: 2021 },
-    { id: 9, concept: "PostgreSQL", experience: 2021 },
-    { id: 10, concept: "Docker", experience: 2023 },
-    { id: 11, concept: "React", experience: 2024 },
-    { id: 12, concept: "Node.js Express", experience: 2024 },
+  { id: 1, concept: "Angular", experience: 2021 },
+  { id: 2, concept: "TypeScript", experience: 2021 },
+  { id: 3, concept: "Java", experience: 2020 },
+  { id: 4, concept: "Spring Boot", experience: 2021 },
+  { id: 5, concept: "HTML/SCSS", experience: 2020 },
+  { id: 6, concept: "C#", experience: 2021 },
+  { id: 7, concept: "Unity", experience: 2021 },
+  { id: 8, concept: "Python", experience: 2021 },
+  { id: 9, concept: "PostgreSQL", experience: 2021 },
+  { id: 10, concept: "Docker", experience: 2023 },
+  { id: 11, concept: "React", experience: 2024 },
+  { id: 12, concept: "Node.js Express", experience: 2024 },
+  { id: 13, concept: "MongoDB", experience: 2024 },
 ];
 
 const StyledWrapper = styled.div`
   .expertise-title {
-      background: linear-gradient(to right,#A855F7, #155799);
-      -webkit-background-clip: text;
-      -webkit-text-fill-color: transparent;
-      width: fit-content;
-  }`;
+    background: linear-gradient(to right, #a855f7, #155799);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    width: fit-content;
+  }
+`;
 
 const About: React.FC = () => {
-    const [currentYear, setCurrentYear] = React.useState(2025);
+  const [currentYear, setCurrentYear] = React.useState(2025);
 
-    useEffect(() => {
-        setCurrentYear(new Date().getFullYear());
-    }, []);
+  useEffect(() => {
+    setCurrentYear(new Date().getFullYear());
+  }, []);
 
-    return (
-        <StyledWrapper>
-            <section id="about" className="min-h-screen flex flex-col items-center bg-base-200 pt-20 pb-10">
-                <motion.div
-                    initial={{ opacity: 0, y: 50 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.5 }}
-                    className="card w-full max-w-3xl bg-base-100 shadow-xl mb-8"
-                >
-                    <div className="card-body flex flex-col md:flex-row items-center">
-                        <div className="flex-1">
-                            <h2 className="card-title text-white">About Me</h2>
-                            <p className="text-white">
-                                Hi, I'm Gustyff – a passionate web & game developer specializing in building exceptional digital experiences.
-                                I enjoy working with modern technologies and creating responsive, interactive websites and fascinating game development concepts.
-                                I am also fascinated by the use of Large Language Models (LLMs) within applications for different use-cases.
-                                <br /><br />
-                                In my free time, I'm accepting freelance jobs with mostly creating full-stack web applications.
-                            </p>
-                        </div>
-                        <div className="flex-shrink-0 mt-4 md:mt-0 md:ml-6">
-                            <div className="avatar">
-                                <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                                    <img src="WJ0Z5RCQ.jpg" alt="Avatar of Gustyff" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </motion.div>
-
-                <div className="container mx-auto px-4">
-                    <h3 className="text-2xl font-bold text-white mb-4">Expertise</h3>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-                        {skills.map((skill) => (
-                            <motion.div
-                                key={skill.id}
-                                initial={{ opacity: 0, y: 20 }}
-                                whileInView={{ opacity: 1, y: 0 }}
-                                whileHover={{ scale: 1.05 }}
-                                viewport={{ once: true }}
-                                transition={{
-                                    delay: skill.id * 0.2,
-                                    type: "spring",
-                                    stiffness: 300,
-                                    damping: 20,
-                                }}
-                                className="card bg-base-100 shadow-md hover:shadow-xl transition-shadow"
-                            >
-                                <div className="card-body">
-                                    <h4 className="card-title expertise-title">{skill.concept}</h4>
-                                    <p className="text-white">
-                                        {currentYear - skill.experience} {(currentYear - skill.experience) === 1 ? "year" : "years"} experience
-                                    </p>
-                                </div>
-                            </motion.div>
-                        ))}
-                    </div>
+  return (
+    <StyledWrapper>
+      <section id="about" className="min-h-screen flex flex-col items-center bg-base-200 pt-20 pb-10">
+        <motion.div initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }} className="card w-full max-w-3xl bg-base-100 shadow-xl mb-8">
+          <div className="card-body flex flex-col md:flex-row items-center">
+            <div className="flex-1">
+              <h2 className="card-title text-white">About Me</h2>
+              <p className="text-white">
+                Hi, I'm Gustyff – a passionate web & game developer specializing in building exceptional digital experiences. I enjoy working with modern technologies and creating responsive, interactive websites and fascinating game development concepts. I am also fascinated by the use of Large Language Models (LLMs) within applications for different use-cases.
+                <br />
+                <br />
+                In my free time, I'm accepting freelance jobs with mostly creating full-stack web applications.
+              </p>
+            </div>
+            <div className="flex-shrink-0 mt-4 md:mt-0 md:ml-6">
+              <div className="avatar">
+                <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                  <img src="WJ0Z5RCQ.jpg" alt="Avatar of Gustyff" />
                 </div>
-            </section>
-        </StyledWrapper>
-    );
+              </div>
+            </div>
+          </div>
+        </motion.div>
+
+        <div className="container mx-auto px-4">
+          <h3 className="text-2xl font-bold text-white mb-4">Expertise</h3>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+            {skills.map((skill) => (
+              <motion.div
+                key={skill.id}
+                initial={{ opacity: 0, y: 20, scale: 1 }}
+                whileInView={{ opacity: 1, y: 0, scale: 1 }}
+                whileHover={{ scale: 1.05 }}
+                viewport={{ once: true }}
+                transition={{
+                  delay: skill.id * 0.2,
+                  scale: { type: "tween", duration: 0.02 },
+                }}
+                className="card bg-base-100 shadow-md hover:shadow-2xl cursor-pointer"
+                style={{ transformOrigin: "center" }}
+              >
+                <div className="card-body">
+                  <h4 className="card-title expertise-title">{skill.concept}</h4>
+                  <p className="text-white">
+                    {currentYear - skill.experience} {currentYear - skill.experience === 1 ? "year" : "years"} experience
+                  </p>
+                </div>
+              </motion.div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </StyledWrapper>
+  );
 };
 
 export default About;
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,6 +8,7 @@ interface Project {
   description: string;
   githubLink?: string;
   tags: string[];
+  isOrder: boolean;
 }
 
 const projects: Project[] = [
@@ -16,12 +17,14 @@ const projects: Project[] = [
     title: "Foxrest",
     description: "A full-stack web application enabling the creation, sharing, and analysis of dynamic forms.  Users can easily build and distribute forms, while administrators gain access to collected data for comprehensive analysis. Developed for freelance clients.",
     tags: ["React", "Node.js Express", "MongoDB", "Docker"],
+    isOrder: true,
   },
   {
     id: 2,
     title: "AIstagram",
     description: 'A full-stack, hobby project reimagining Instagram with AI-powered content.  LLM multimodal models act as "users," generating posts for human consumption.',
     tags: ["Python", "Spring Boot", "React"],
+    isOrder: false,
   },
   {
     id: 3,
@@ -29,6 +32,7 @@ const projects: Project[] = [
     description: 'Terrors of Infinity is a chilling horror game developed as a university thesis project.  Navigate an ever-shifting, procedurally generated maze while evading "The Shifter," a terrifying and unpredictable entity.  Experience the dread of infinite pursuit in this unique survival horror experience.',
     githubLink: "https://github.com/denisz-becskei/terrors_of_infinity",
     tags: ["Unity", "C#"],
+    isOrder: false,
   },
   {
     id: 4,
@@ -36,6 +40,7 @@ const projects: Project[] = [
     description: 'A visually captivating simulation of flocking behavior. Experience the emergent patterns of simplified bird-like agents ("Boids") as they interact and move according to a classic flocking algorithm.  More of a dynamic art piece than a traditional game.',
     githubLink: "https://github.com/denisz-becskei/boids",
     tags: ["Unity", "C#"],
+    isOrder: false,
   },
   {
     id: 5,
@@ -43,12 +48,14 @@ const projects: Project[] = [
     description: "A novel application generating multi-colored QR codes for enhanced data capacity.  Utilizing red, green, and blue layers, this system effectively triples the data density of traditional QR codes, read by a dedicated multispectral scanner.",
     githubLink: "https://github.com/denisz-becskei/multispectral-qr-code",
     tags: ["Python"],
+    isOrder: false,
   },
   {
     id: 6,
     title: "Adventures of the Blobs",
     description: 'A vibrant MMORPG where players control customizable "Blob" characters, each with unique classes, abilities, and spells.  Engage in epic battles against challenging raid bosses to earn valuable loot and forge your legend.',
     tags: ["Unity", "C#"],
+    isOrder: false,
   },
   {
     id: 7,
@@ -56,30 +63,35 @@ const projects: Project[] = [
     description: "Transform your images into Minecraft-style block art.  This application analyzes your image, comparing each pixel's color to a user-defined block palette.  It then replaces each pixel with the closest matching block color, creating a blockified version of your image.",
     githubLink: "https://github.com/denisz-becskei/blockify",
     tags: ["Python"],
+    isOrder: false,
   },
   {
     id: 8,
     title: "CodeForge",
     description: "A full-stack web application leveraging locally run Large Language Models (LLMs) to deliver valuable insights and assistance to programmers.",
     tags: ["React", "Node.js Express", "SQLite", "Docker"],
+    isOrder: false,
   },
   {
     id: 9,
     title: "Insight & Insight Woe",
     description: "A mobile face-swapping app using InSwapper and Codeformer AI models for realistic face replacement and restoration.",
     tags: ["Python", "Kotlin"],
+    isOrder: false
   },
   {
     id: 10,
     title: "RAGStage",
     description: "A full-stack application leveraging Retrieval Augmented Generation (RAG) to provide accurate answers to user questions based on uploaded document content.",
     tags: ["Spring Boot", "Angular", "Typescript", "PostgreSQL", "Docker"],
+    isOrder: false,
   },
   {
     id: 11,
     title: "Dark Graph",
     description: "A full-stack application for exploring the Deep Web.  Leveraging GraphScraper, GraphStore, and GraphEvaluator modules, Dark Graph scrapes and visualizes Deep Web content in an interactive, chart-based system, enabling one-click exploration.",
     tags: ["Python", "MongoDB", "Docker"],
+    isOrder: false,
   },
   {
     id: 12,
@@ -87,12 +99,28 @@ const projects: Project[] = [
     description: "A classic implementation of Conway's Game of Life cellular automaton.",
     githubLink: "https://github.com/denisz-becskei/conwaysgameoflife",
     tags: ["HTML/CSS", "JavaScript"],
+    isOrder: false,
   },
   {
     id: 13,
     title: "Grimoire",
     description: "A web application built with React and Node.js Express that streamlines employee leave management. It allows employees to request leaves, and provides HR and project managers with a centralized system to review, approve, or deny these requests, ensuring efficient tracking and notification.",
     tags: ["React", "Node.js Express", "PostgreSQL", "Docker"],
+    isOrder: true,
+  },
+  {
+    id: 14,
+    title: "VTMv5: Beyond",
+    description: "A character management platform for the tabletop game Vampire: The Masquarade. It features character building, management, and physics based dice rolling via Three.js.",
+    tags: ["React", "Node.js Express", "PostgreSQL", "Docker"],
+    isOrder: false
+  },
+  {
+    id: 15,
+    title: "The Zero Engine",
+    description: "A Reddit-style forum clone with AI Agents talking to one another. Many things can be configured, such as interaction rate, action type, automatic content creation, etc.",
+    tags: ["React", "Node.js Express", "MongoDB", "Docker", "Ollama"],
+    isOrder: false,
   },
 ];
 
@@ -105,26 +133,27 @@ const Projects: React.FC = () => {
           {projects.map((project) => (
             <motion.div
               key={project.id}
-              initial={{ opacity: 0, y: 20 }}
-              whileInView={{ opacity: 1, y: 0 }}
+              initial={{ opacity: 0, y: 20, scale: 1 }}
+              whileInView={{ opacity: 1, y: 0, scale: 1 }}
               whileHover={{ scale: 1.05 }}
               viewport={{ once: true }}
               transition={{
                 delay: project.id * 0.2,
-                type: "spring",
-                stiffness: 300,
-                damping: 20,
+                scale: { type: "tween", duration: 0.02 },
               }}
-              className="card bg-base-200 shadow-md"
+              className="card bg-base-200 shadow-md hover:shadow-2xl cursor-pointer"
+              style={{ transformOrigin: "center" }}
             >
               <div className="card-body">
                 <div className="flex w-full justify-between">
                   <h3 className="card-title text-white">{project.title}</h3>
+                  {project.isOrder && (
                   <Tooltip title="Available for ordering" placement="top" arrow>
                     <span id={"project-" + project.id} data-tooltip-id={"project-" + project.id} data-tooltip-content="Available for ordering" className="flex cursor-crosshair">
                       *
                     </span>
                   </Tooltip>
+                  )}
                 </div>
                 <p className="text-white">{project.description}</p>
                 <div className="flex flex-wrap gap-2 mt-2">
